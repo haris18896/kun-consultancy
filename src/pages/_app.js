46ae6702c9c72d2457172891a1b85fc47991d802
haris18896 <haris18896@gmail.com>
2022-06-25 00:27:25 +0500

    nprogress bar color chaned globally

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,34 @@
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
-import Head from 'next/head';
-import { ScrollToTop } from '../components/scroll';
+import '../styles/globals.css'
+import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import Layout from '../components/layout/layout'
+
+import { wrapper } from '../redux/store'
+import { ScrollToTop } from '../components/scroll'
+
+NProgress.configure({ showSpinner: false })
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Layout>
-            <Head>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1'
-                />
-                <link rel='shortcut icon' href='../favicon.png' />
-            </Head>
-            <Component {...pageProps} />
-            <ScrollToTop />
-        </Layout>
-    );
+    
+    Router.events.on('routeChangeStart', url => {
+    NProgress.start(url)
+  })
+  Router.events.on('routeChangeComplete', url => {
+    NProgress.done(url)
+  })
+
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='shortcut icon' href='../favicon.png' />
+      </Head>
+      <Component {...pageProps} />
+      <ScrollToTop />
+    </Layout>
+  )
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp)
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang='en' className='scroll-smooth'>
-                <Head />
-                <body className='font-inter relative'>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang='en' className='scroll-smooth'>
+        <Head>
+          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' />
+        </Head>
+        <body className='font-inter relative'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument;
+export default MyDocument
