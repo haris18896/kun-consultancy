cc1c454b0d1f5032ca07b3e97039819e178f6608
haris18896 <haris18896@gmail.com>
2022-07-22 02:00:54 +0500

    :construction: some error
    in mail

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -4,19 +4,30 @@ import sgMail from '@sendgrid/mail'
 
 sgMail.setApiKey(process.env.EMAIL_API_KEY)
 
-export default (req, res) => {
+export default async (req, res) => {
   const body = req.body
+  console.log('body', body)
 
   const message = `
         Name: ${body.name}\r\n
         Email: ${body.email}\r\n
+        Subject: ${body.subject}\r\n
         Message: ${body.message}
     `
 
   const data = {
     to: 'kunconsultancy2022@gmail.com',
     from: body.email,
+    subject: body.subject,
     name: body.name,
-    text: body.message
+    text: body.message,
+    html: message.replace(/\r\n/g, '<br />')
+  }
+
+  try {
+    await sgMail.send(data)
+    res.json({ message: `Email has been sent` })
+  } catch (error) {
+    res.status(500).json({ error: 'Error sending email' })
   }
 }
