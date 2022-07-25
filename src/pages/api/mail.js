63c69bbcccfddbef76cd7eb090c7b09c5ba5b9d5
haris18896 <haris18896@gmail.com>
2022-07-25 21:56:08 +0500

    still showing problem on username and password

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -7,20 +7,26 @@ dotenv.config()
 export default async (req, res) => {
   const { name, email, subject, message } = req.body
 
-  const transporter = nodemailer.createTransport({
+  let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
+      type: 'OAUTH2',
+      user: process.env.GMAIL_USERNAME,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      accessToken: process.env.OAUTH_ACCESS_TOKEN,
+      expires: 3599
     }
   })
 
   const mailData = {
     from: 'kunconsultancy2022@gmail.com',
     to: 'hk6530739@gmail.com',
+    // to: "info@kunconsultingservices.com"
     name: `Message From ${name}`,
     subject: `Message From ${subject}`,
     text: `This mail is sent by ${email}\r\n${message}`,
@@ -31,8 +37,8 @@ export default async (req, res) => {
     transporter.sendMail(mailData, function (err, info) {
       if (err) console.log('error in nodemailer:-', err)
       else console.log('info', info)
+      if (!err) res.status(200).json({ msg: 'Email Sent successfully' })
     })
-    res.status(200).json({ msg: 'Email Sent successfully' })
   } catch (error) {
     console.log('error 500:- ', error)
     res.status(500).json({ msg: 'Error sending email' })
